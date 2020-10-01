290925914740b80ba144033c9252d5f27023ade9
Peter Inglesby <peter.inglesby@gmail.com>
2020-10-01 22:28:03 +0100

    Simplify implementation of tree_tables

--- codelists/presenters.py
+++ codelists/presenters.py
@@ -69,31 +69,21 @@ def build_definition_rows(coding_system, hierarchy, definition):
     return list(_iter_rules(hierarchy, included_rules, name_for_rule, excluded_rules))
 
 
-def tree_tables(codes_by_type, hierarchy, code_to_term):
+def tree_tables(ancestor_codes_by_type, hierarchy, code_to_term):
     """
     Return list of tables of codes arranged in trees, grouped by type of code.
 
     Each table is a dict, with a heading and a list of rows.
     """
 
-    sort_by_term_key = code_to_term.__getitem__
-
     tables = []
 
-    for type, codes_for_type in sorted(codes_by_type.items()):
+    for type, codes_for_type in sorted(ancestor_codes_by_type.items()):
         rows = []
 
-        for ancestor_code in sorted(codes_for_type, key=sort_by_term_key):
-            for code, pipes in hierarchy.walk_depth_first_as_tree_with_pipes(
-                starting_node=ancestor_code, sort_key=sort_by_term_key
-            ):
-                rows.append(
-                    {
-                        "code": code,
-                        "term": code_to_term[code],
-                        "pipes": pipes,
-                    }
-                )
+        for ancestor_code in sorted(codes_for_type, key=code_to_term.__getitem__):
+            for row in tree_rows(ancestor_code, hierarchy, code_to_term):
+                rows.append(row)
 
         table = {
             "heading": type.title(),
@@ -102,3 +92,32 @@ def tree_tables(codes_by_type, hierarchy, code_to_term):
         tables.append(table)
 
     return tables
+
+
+def tree_rows(ancestor_code, hierarchy, code_to_term):
+    """
+    Return list of rows for a tree comprising an ancestor and all its descendants in
+    depth-first order.  If the hierarchy is a graph, codes will appear once per path
+    from the ancestor to the code.
+    """
+
+    def helper(code, pipes):
+        yield {"code": code, "term": code_to_term[code], "pipes": pipes}
+
+        if pipes:
+            *first_pipes, last_pipe = pipes
+            if last_pipe == "└":
+                pipes = first_pipes + [" "]
+            else:
+                pipes = first_pipes + ["│"]
+
+        children = hierarchy.child_map.get(code, [])
+        for ix, child in enumerate(sorted(children, key=code_to_term.__getitem__)):
+            if ix == len(children) - 1:
+                pipe = "└"
+            else:
+                pipe = "├"
+
+            yield from helper(child, pipes + [pipe])
+
+    yield from helper(ancestor_code, [])
